fc5585fc890308844e1056a1470e815aea0ac7fd
li.ming <li.ming@topka.cn>
2015-07-03 17:02:18 +0800

    update

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 # auto install salt-minion
 hostname=`hostname`
 yum install salt-minion -y
-sed -i 's/^#master: salt*/master: 172.16.6.4/' /etc/salt/minion
+sed -i "s/^#master: salt*/master: $1/" /etc/salt/minion
 sed -i "s/^#id:/id: $hostname/g" /etc/salt/minion
 service salt-minion start

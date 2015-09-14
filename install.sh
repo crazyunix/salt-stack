0fd34da48dd43d09cc29578b63707ef3517f8895
Crazyunix <crazyunix@163.com>
2015-09-14 15:39:14 +0800

    delete install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# auto install salt-minion
-rpm -Uvh http://ftp.linux.ncsu.edu/pub/epel/6/i386/epel-release-6-8.noarch.rpm
-hostname=`hostname`
-yum install salt-minion -y
-sed -i "s/^#master: salt*/master: $1/" /etc/salt/minion
-sed -i "s/^#id:/id: $hostname/g" /etc/salt/minion
-service salt-minion start

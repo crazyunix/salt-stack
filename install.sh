1141eae32c5896bfaabd60e580b13121b0db47ca
root <root@bj-web-08.(none)>
2015-09-14 15:13:10 +0800

    add salt yum epel

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # auto install salt-minion
+rpm -Uvh http://ftp.linux.ncsu.edu/pub/epel/6/i386/epel-release-6-8.noarch.rpm
 hostname=`hostname`
 yum install salt-minion -y
 sed -i "s/^#master: salt*/master: $1/" /etc/salt/minion

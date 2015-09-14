691cc76e9ec2d6fcbc101e744e214a93fb1215ce
Crazyunix <crazyunix@163.com>
2015-09-14 15:42:06 +0800

    update

--- salt/install.sh
+++ salt/install.sh
@@ -4,6 +4,6 @@
 rpm -Uvh http://ftp.linux.ncsu.edu/pub/epel/6/i386/epel-release-6-8.noarch.rpm
 hostname=`hostname`
 yum install salt-minion -y
-sed -i "s/^#master: salt*/master: $1/" /etc/salt/minion
-sed -i "s/^#id:/id: $hostname/g" /etc/salt/minion
+sed -i "s/^#master: salt*/ master: $1/" /etc/salt/minion
+sed -i "s/^#id:/ id: $hostname/g" /etc/salt/minion
 service salt-minion start
